c37e05146ad6b12a53763ea4c6d0c1141da1f742
ConstDet <constx7@gmail.com>
2023-02-26 20:24:00 +0300

    Список list отсортирован по фамилии и возрасту

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("Peter", "Rumyantsev-Zadunaisky", 298));
+        list.add(new Person("Grigory", "Potemkin-Tauride", 284));
+        list.add(new Person("Mikhail", "Golenishchev-Kutuzov-Smolensky", 278));
+        list.add(new Person("Alexander", "Pushkin", 224));
+        System.out.println(list);
+        Collections.sort(list, new PersonComparator());
+        System.out.println(list);
     }
 }
